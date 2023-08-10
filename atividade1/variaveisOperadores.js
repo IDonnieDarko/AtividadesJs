8f79d3aa8a6698e810cf0452f90630b629b7c380
Savio <savio.sfvfv@gmail.com>
2023-08-10 11:21:16 -0300

    atividade1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,25 @@
+//variáveis
+let numeroA1 = 50
+let numeroB1 = 80
+//Variáveis de divisão e multiplicação
+let numeroC1 = 50
+let numeroD1 = 20
+
+//Soma e subtração
+let soma = numeroA1 + numeroB1
+let subtraction = numeroA1 - numeroB1
+
+//Divisão e Multiplicação
+let multiplication = numeroC1 * numeroD1
+let division = numeroC1 / numeroD1
+
+//resultado soma e subtração
+console.log(`O resultado da operação 50 + 80 é ${soma}`)
+console.log(`O resultado da subtração 50 - 80 é ${subtraction}`)
+
+//resultado multiplicação e divisão
+console.log(`O resultado da multiplicação 50 * 20 é ${multiplication}`)
+console.log(`O resultado da divisão 50 / 20 é ${division}`)
+
+
+
